314e86d3a58ceadaede297579e492f6379209570
ivanam <iva.moyano@gmail.com>
2019-02-23 20:53:17 -0300

    modifico los gemfile

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+#Gem::Specification.find_by_name('bundler', '~> 1.17.3').activate
 require 'bundler/setup' # Set up gems listed in the Gemfile.
